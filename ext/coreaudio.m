25f632b0997ae6c079b5eb609852c037be7764c0
shuhei kinukawa <kinushu@gmail.com>
2014-01-19 23:36:11 +0900

    Add feature to set default output device

--- ext/coreaudio.m
+++ ext/coreaudio.m
@@ -417,6 +417,34 @@ ca_default_output_device(int argc, VALUE *argv, VALUE mod)
 }
 
 /*
+ * Document-method: CoreAudio.set_default_output_device
+ * call-seq:
+ *   CoreAudio.set_default_output_device(audio_device)
+ *
+ * Set system default audio output device as CoreAudio::AudioDevice object.
+ */
+static VALUE
+ca_set_default_output_device(VALUE self, VALUE device)
+{
+    AudioDeviceID devID = NUM2UINT(rb_ivar_get(device, sym_iv_devid));    
+    AudioObjectPropertyAddress address = PropertyAddress;
+    UInt32 size;
+    OSStatus status;
+
+    address.mSelector = kAudioHardwarePropertyDefaultOutputDevice;
+    address.mScope    = kAudioObjectPropertyScopeGlobal;
+    address.mElement  = kAudioObjectPropertyElementMaster;
+
+    status = AudioObjectSetPropertyData(kAudioObjectSystemObject, &address, 0, NULL, sizeof(devID), &devID);
+    if (status != noErr) {
+      rb_raise(rb_eArgError,
+               "coreaudio: set default output deveice failed: %d", status);
+    }
+
+    return Qtrue;
+}
+
+/*
  * Document-class: CoreAudio::OutLoop
  *
  * CoreAudio::OutLoop is an class for loop waveform to output audio stream.
@@ -1130,6 +1158,7 @@ Init_coreaudio_ext(void)
     rb_define_singleton_method(rb_mCoreAudio, "devices", ca_devices, -1);
     rb_define_singleton_method(rb_mCoreAudio, "default_input_device", ca_default_input_device, -1);
     rb_define_singleton_method(rb_mCoreAudio, "default_output_device", ca_default_output_device, -1);
+    rb_define_singleton_method(rb_mCoreAudio, "set_default_output_device", ca_set_default_output_device, 1);
 
     rb_define_method(rb_cOutLoop, "[]=", ca_out_loop_data_assign, 2);
     rb_define_method(rb_cOutLoop, "start", ca_out_loop_data_start, 0);
